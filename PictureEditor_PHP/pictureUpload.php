c1cf8fd173fa3e30f2d667e8b473ccfef35d72d6
Lyman <limianfeng12@gmail.com>
2016-01-22 14:52:24 +0800

    解决服务器只能保存一份图片

--- PictureEditor_PHP/pictureUpload.php
+++ PictureEditor_PHP/pictureUpload.php
@@ -9,30 +9,32 @@
 	$edit_type = explode("@",$file_name)[0];
 	$file_name = explode("@",$file_name)[1];
 
-	$target_path = $target_path.basename( $file_name );
+	$now_time = time();
+
+	$target_path = $target_path.$now_time."_".basename( $file_name );
 
 	if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
 
 		switch ($edit_type) {
 			case '1':
-				grayscale('./upload/'.$file_name, './edit/'.$file_name);
+				grayscale('./upload/'.$now_time."_".$file_name, './edit/'.$now_time."_".$file_name);
 				break;
 			case '2':
-				blur('./upload/'.$file_name, './edit/'.$file_name);
+				blur('./upload/'.$now_time."_".$file_name, './edit/'.$now_time."_".$file_name);
 				break;
 			case '3':
-				gammaCorrection('./upload/'.$file_name, './edit/'.$file_name);
+				gammaCorrection('./upload/'.$now_time."_".$file_name, './edit/'.$now_time."_".$file_name);
 				break;
 			case '4':
-				colorize('./upload/'.$file_name, './edit/'.$file_name);
+				colorize('./upload/'.$now_time."_".$file_name, './edit/'.$now_time."_".$file_name);
 				break;
 			case '5':
-				imageWatermarking('./upload/'.$file_name, './edit/'.$file_name);
+				imageWatermarking('./upload/'.$now_time."_".$file_name, './edit/'.$now_time."_".$file_name);
 				break;
 			default:
 				break;
 		}
-		$url = "/edit/".$file_name;
+		$url = "/edit/".$now_time."_".$file_name;
 	   	echo $url;
 	}  else {
 
